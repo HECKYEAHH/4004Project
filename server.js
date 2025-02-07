24cf33f8604076c5ab403dece20dc45ab24c009e
HECKYEAHH <rj3noone@gmail.com>
2025-02-06 22:29:34 -0500

    Changed path in server.js file

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ const path = require("path");
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname,"Login", "index.html"));
 });
 
   //TEST
