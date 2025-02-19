168356153677955960c87543cf3c49f7dd63205e
HECKYEAHH <ri422982@ucf.edu>
2025-02-19 15:11:43 -0500

    fixed reset password.html

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.get('/forgot-password.html', (req, res) => {
 });
 
 //serve actual reset password
-app.get('/reset-password', (req, res) => {
+app.get('/reset-password.htyml', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'views', 'reset-password.html'));
 });
 
