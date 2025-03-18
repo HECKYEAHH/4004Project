08f707eba8b408ee61f9fff1925652c53d1c2aa3
HECKYEAHH <ri422982@ucf.edu>
2025-03-18 19:47:02 -0400

    server.js

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.get('/forgot-password.html', (req, res) => {
 app.get('/reset-password.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'views', 'reset-password.html'));
 });
-app.get('/idle_game', (req, res) => {
+app.get('/idle_game.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'views', 'idle_game.html'));
 });
 
